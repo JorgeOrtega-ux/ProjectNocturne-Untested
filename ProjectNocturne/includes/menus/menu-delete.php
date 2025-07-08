1c3f4b107bfed6108e1c168e8543d1bbb9d7df84
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-07-08 14:01:09 -0500

    Add files via upload

--- ProjectNocturne/includes/menus/menu-delete.php
+++ ProjectNocturne/includes/menus/menu-delete.php
@@ -3,20 +3,28 @@
         <div class="drag-handle"></div>
     </div>
     <div class="menu-section">
-        <div class="menu-content-scrolleable">
-            <div class="menu-section-center">
-                <div class="menu-content-wrapper active">
-                    <div class="menu-content">
-                        <div class="menu-content-general">
-                            <h1></h1>
-                            <span></span>
-                            <div class="menu-delete-btns">
-                                <button class="cancel-btn body-title"></button>
-                                <button class="confirm-btn body-title"></button>
-                            </div>
-                        </div>
+        <div class="menu-section-top">
+            <div class="menu-content-header">
+                <div class="menu-content-header-primary">
+                    <span data-translate="confirm_delete_title_alarm" data-translate-category="confirmation">Confirmar eliminación</span>
+                </div>
+            </div>
+        </div>
+        <div class="menu-content-scrolleable overflow-y">
+        <div class="menu-section-center">
+            <div class="menu-content-wrapper active">
+                <div class="menu-content">
+                    <div class="menu-content-general">
+                        <span>Estás a punto de eliminar la alarma <strong>rrrr</strong>. Esta acción no se puede deshacer.</span>
                     </div>
                 </div>
+            </div>
+            </div>
+        </div>
+        <div class="menu-section-bottom">
+            <div class="menu-delete-btns">
+                <button class="cancel-btn body-title" data-translate="cancel" data-translate-category="confirmation">Cancelar</button>
+                <button class="confirm-btn body-title" data-translate="delete" data-translate-category="confirmation">Eliminar</button>
             </div>
         </div>
     </div>
